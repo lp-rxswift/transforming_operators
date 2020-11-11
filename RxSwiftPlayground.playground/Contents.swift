24b2cf89c13895f955ab0758cf509ea24bf6b361
Lucas <lucaspedrazoli@gmail.com>
2020-11-11 13:55:52 -0300

    add toArray() example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -1,6 +1,11 @@
 import Foundation
 import RxSwift
 
-example(of: "Test") {
+example(of: "toArray") {
+  let disposeBag = DisposeBag()
 
+  Observable.of("A", "B", "C")
+    .toArray()
+    .subscribe(onSuccess: { print($0) })
+    .disposed(by: disposeBag)
 }
